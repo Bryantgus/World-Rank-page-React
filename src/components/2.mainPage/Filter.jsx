e742edc40c2e747e1a9d65c05416322d4021861b
Bryant <bryanttejeda628f@hotmail.com>
2024-12-28 12:01:13 -0400

    Trabanjando en la parte de filtros html css

--- src/components/2.mainPage/Filter.jsx
+++ src/components/2.mainPage/Filter.jsx
@@ -3,14 +3,20 @@ import "./Filter.css"
 export default function Filter() {
     return (
         <div className="filter">
-            <div className="sortBy">
-                <span>Sort by</span>   
+
+            <div>
+                <span className="titlesFilter">Sort by</span>   
                 <select className="custom-select">
                     <option value="1">Population</option>
                     <option value="2">Area (km²)</option>
                     <option value="3">Name</option>
                 </select>
             </div>
+
+            <div className="region titlesFilter">
+                <span className="titlesFilter">Region</span>
+            </div>
+
              
 
         </div>
